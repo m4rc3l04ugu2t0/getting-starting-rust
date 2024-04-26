7ad39a9fc6510394961a091e10c9d1106b6204c2
Marcelo <mrcelo014@gmail.com>
2024-04-26 20:59:14 -0300

    Variáveis e constantes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+const SECONDS_IN_MINUTE: u32 = 60;
+const MINUTE_IN_HOUR: u32 = 60;
+const SECONDS_IN_HOUR: u32 = SECONDS_IN_MINUTE * MINUTE_IN_HOUR;
+
 fn main() {
-    println!("Hello, world!");
+    let total = 30;
+    println!("trabalhou {total} horas.");
+    {
+        let total = total - 20;
+        println!("Não trabalhou {total} horas.");
+    }
+    let total = 44;
+    println!("trabalhou {total} horas.");
+    let total_in_seconds = total * SECONDS_IN_HOUR;
+    println!("trabalhou {total_in_seconds} segundos.");
 }
+// RAII
+
